fb7d370afdfdbf4eb8206eadc5adf50615c354e2
Amodias <saidamokranebelgacem@gmail.com>
2022-06-22 11:18:47 +0000

    popup success added with correct jquery and bootstrap import

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -7,8 +7,17 @@ import VueMeta from 'vue-meta'
 
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'jquery'
+
+Vue.use({
+  install: function(Vue, options){
+      Vue.prototype.$jQuery = require('jquery'); // you'll have this.$jQuery anywhere in your vue project
+      }
+  });
 
 Vue.config.productionTip = false
 // Make BootstrapVue available throughout your project
